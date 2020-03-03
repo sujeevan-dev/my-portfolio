6f1b95c97772d81e6cc4cd0e4f1fdce59784aa40
sujeevan <psujeevan@gmail.com>
2020-03-03 23:46:20 +0530

    Updates

---
+++ precache-manifest.c3367189a791b1d60666148307aa09cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084f68d4c7ccbef4c3aab98c32fedb12",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "e3c8b3753153854a7d45",
+    "url": "/my-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4c18c16231221e12405c",
+    "url": "/my-portfolio/static/js/2.2717102d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-portfolio/static/js/2.2717102d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c8b3753153854a7d45",
+    "url": "/my-portfolio/static/js/main.28ad4301.chunk.js"
+  },
+  {
+    "revision": "7c7d8426a5976bd5dcaf",
+    "url": "/my-portfolio/static/js/runtime-main.ec1bad13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
